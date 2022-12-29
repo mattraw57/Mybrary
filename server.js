89d0f358568315579c08b834df06cea586f37b80
Matthew Raw <mraw1@sheffield.ac.uk>
2022-12-29 21:09:34 +0000

    bug

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
-  }
+}
+
+  const path = require('path') 
   
   const express = require('express')
   const app = express()
@@ -9,7 +11,7 @@ if (process.env.NODE_ENV !== 'production') {
   const indexRouter = require('./routes/index')
   
   app.set('view engine', 'ejs')
-  app.set('views', __dirname + '/views')
+  app.set('views', path.join(__dirname, 'views'))
   app.set('layout', 'layouts/layout')
   app.use(expressLayouts)
   app.use(express.static('public'))
