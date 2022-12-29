c7ea5bc9d43226e3d4c17515e15493adb64514e2
Matthew Raw <mraw1@sheffield.ac.uk>
2022-12-29 23:30:03 +0000

    initial author routes

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+//View site at: https://mybrary-app-lytr.onrender.com/
+
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
 }
@@ -7,15 +9,18 @@ if (process.env.NODE_ENV !== 'production') {
   const express = require('express')
   const app = express()
   const expressLayouts = require('express-ejs-layouts')
-  
+  const bodyParser = require('body-parser')
+
   const indexRouter = require('./routes/index')
+  const authorRouter = require('./routes/authors')
   
   app.set('view engine', 'ejs')
   app.set('views', path.join(__dirname, 'views'))
   app.set('layout', 'layouts/Layout')
   app.use(expressLayouts)
   app.use(express.static('public'))
-  
+  app.use(bodyParser.urlencoded({ limit: '10mb', extended: false}))
+
   const mongoose = require('mongoose')
   mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
   const db = mongoose.connection
@@ -23,5 +28,6 @@ if (process.env.NODE_ENV !== 'production') {
   db.once('open', () => console.log('Connected to Mongoose'))
   
   app.use('/', indexRouter)
+  app.use('/authors', authorRouter) //every route inside autoRouter is prepended with 'authors'
   
   app.listen(process.env.PORT || 3000)
